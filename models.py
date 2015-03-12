8b1f77fddfe5f6ac42e916dbf6f660e1ea994b4a
David Bertha <dbertha@ulb.ac.be>
2015-03-12 13:35:23 +0100

    style skeletton

---
+++ models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class fonteyne_style(models.Model):
+#     _name = 'fonteyne_style.fonteyne_style'
+
+#     name = fields.Char()
