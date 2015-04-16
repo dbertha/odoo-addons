38b41d557df6bb6297cde0c2543e15989096e4bd
David Bertha <dbertha@ulb.ac.be>
2015-04-16 11:11:21 +0200

    redirecting when cart total is less than 5€

--- fonteyne_style/models.py
+++ fonteyne_style/models.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api
+from openerp.osv import orm, fields
+import logging
 
+_logger = logging.getLogger(__name__)
 # class fonteyne_style(models.Model):
 #     _name = 'fonteyne_style.fonteyne_style'
 
 #     name = fields.Char()
+
+class ProductCategory(orm.Model):
+    _inherit = 'product.category'
+    
+    def hierarchy_selected(self, cr, uid, ids, activeCategoryNumber, context=None):
+        _logger.debug("hierarchy selected, number : %d", activeCategoryNumber)
+        if not isinstance(activeCategoryNumber, int) :
+            return False
+                                                     
+        for category in self.browse(cr, uid, ids, context=context) :
+            if category.id == activeCategoryNumber : 
+                return True
+            else :
+                return any(child.id == activeCategoryNumber for child in category.child_id)
