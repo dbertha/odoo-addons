8c29609cb85943c0f0729fc4831fd071f3e62013
David Bertha <dbertha@ulb.ac.be>
2015-06-24 13:01:08 +0200

    [rotating products] new table delivery.condition to link product.public.cat with certains delivery.carrier and delay rules. Shop page doesn't show products incompatibles (not the same delivery.condition) with what's in the cart

--- website_sale_rotating/models.py
+++ website_sale_rotating/models.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from openerp import SUPERUSER_ID
 from openerp import models
 from openerp.osv import osv, fields
+from openerp.addons.web.http import request
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -98,3 +100,104 @@ class product_template(osv.Model):
         """Expect only one ID"""
         for product in self.browse(cr, uid, ids, context=context) :
             return product.week_number
+        
+class delivery_condition(osv.Model):
+    #TODO : incompatibility between categories but product can be from multiple categ : proper ?
+    _name = "delivery.condition"
+    _description = "Delivery Condition : delivery carrier and delays compatible"
+     
+    _columns = {
+        'name': fields.char('Delivery Condition Name', required=True),
+        #'partner_id': fields.many2one('res.partner', 'Transport Company', required=True, help="The partner that is doing the delivery service."),
+        #'product_id': fields.many2one('product.product', 'Delivery Product', required=True),
+        'category_ids': fields.one2many('product.public.category', 'condition_id', string='Public Categories'),
+        #TODO : conditions_id : 'carrier_id': fields.many2one('delivery.carrier', 'Carrier', required=True, ondelete='cascade')
+        'carrier_ids' : fields.one2many('delivery.carrier', 'condition_id', string='Delivery Carriers'),
+        'delay_from' : fields.integer(string="Delay in days from the present as minimum date"),
+        'delay_to' : fields.integer(string="Delay in days from the minimum date as maximum date"),
+        'limit_to_a_range_of_days' : fields.boolean(
+                        string="Delivery date proposition should be limited to the range",
+                        help="Max and min dates will be computed from there and delay from"),
+        'range_start' : fields.selection(
+                [(1, 'Monday'), (2, 'Tuesday'), (3, 'Wednesday'), (4, 'Thurday'), 
+                 (5, 'Friday'), (6, 'Saturday'), (7, 'Sunday')], #1-7 because no selection = 0
+                string="First day of the allowed range"
+                ),
+    #fields.integer(string="First day of the allowed range (0-monday, 6-sunday)"),
+        'range_end' : fields.selection(
+                [(1, 'Monday'), (2, 'Tuesday'), (3, 'Wednesday'), (4, 'Thurday'), 
+                 (5, 'Friday'), (6, 'Saturday'), (7, 'Sunday')], #1-7 because no selection = 0
+                string="Last day of the allowed range"
+                )
+    #fields.integer(string="Last day of the allowed range (0-monday, 6-sunday)")
+        #possible improvement : 'repeat_interval'
+        #cas : 
+        #normal : min = now + 1h
+        #events : min = now + 1 day + 1 day if after 17h
+        #FFY : samedi à mardi
+    }
+    #TODO : in a sale order, only one delivery_conditions represented
+    #TODO : gestion propre du débordement de l'interval à la semaine suivante'
+    
+class product_public_category(osv.osv):
+    _name = 'product.public.category'
+    _inherit = "product.public.category"
+    _columns = {
+        'condition_id' : fields.many2one('delivery.condition', string="Conditions of Delivery")
+    }
+    
+class delivery_carrier(osv.osv):
+    _name = "delivery.carrier"
+    _inherit = "delivery.carrier"
+    _columns = {
+        'condition_id' : fields.many2one('delivery.condition', string="Conditions of Delivery")
+    }
+    
+class sale_order(osv.osv):
+    _name = "sale.order"
+    _inherit = "sale.order"
+    
+    _columns = {
+        'delivery_condition' : fields.related('order_line', 
+            'product_id', 'product_tmpl_id', 'public_categ_ids', 'condition_id', 
+            type="many2one", relation="delivery.condition", string="Delivery Condition")
+    } 
+    """this field will provide a link to the delivery condition of the first public categ of the first product
+        Expect only one type of delivery condition in the sale order
+        i.e. all the categories of the products in sale order lines have
+        the same delivery condition"""
+        
+class Website(osv.osv):
+    _inherit = 'website'
+    
+    def sale_product_domain(self, cr, uid, ids, context=None):
+        """Remove objects from categories with another delivery condition than the sale order"""
+        domain = super(Website, self).sale_product_domain(cr, uid, ids, context=context)
+        sale_order_obj = self.pool['sale.order']
+        sale_order_id = request.session.get('sale_order_id')
+        _logger.debug("sale product domain overload")
+        if sale_order_id :
+            sale_order = sale_order_obj.browse(cr, SUPERUSER_ID, sale_order_id, context=context)  
+            if sale_order.exists() :
+                _logger.debug("sale order exists")
+                if sale_order.delivery_condition :
+                    #find the acceptable categs
+                    categs_ids = self.pool['product.public.category'].search(cr,uid, 
+                            [('condition_id', '=', sale_order.delivery_condition.id)], context=context)
+                    
+                    _logger.debug("sale order condition : %d", sale_order.delivery_condition.id)
+                    domain += [('public_categ_ids','in',categs_ids)]
+        return domain
+    
+#     def get_delivery_condition(self,cr,uid,order_id,context=None) :
+#         """Expect only one type of delivery condition in the sale order
+#         i.e. all the categories of the products in sale order lines have
+#         the same delivery condition
+#         Expect only one order_id"""
+#         for order in self.browse(cr, uid, [order_id], context) :
+#             for line in order.order_line :
+#                 if line.product_id :
+#                     public_categs =line.product_id.product_tmpl_id.public_categ_ids 
+#                     if public_categs :
+#                         return public_categs[0].condition_id
+#                     
