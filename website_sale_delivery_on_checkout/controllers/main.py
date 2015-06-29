e110f8ec488a0b6358ddbfd1c1ddba87781a2bab
David Bertha <dbertha@ulb.ac.be>
2015-06-29 09:36:00 +0200

    fix : test if carrier_id before reading name

--- website_sale_delivery_on_checkout/controllers/main.py
+++ website_sale_delivery_on_checkout/controllers/main.py
@@ -16,7 +16,7 @@ class website_sale(openerp.addons.website_sale.controllers.main.website_sale):
     @http.route(['/shop/checkout'], type='http', auth="public", website=True)
     def checkout(self, **post):
         cr, uid, context = request.cr, request.uid, request.context
-
+        _logger.debug("Request for checkout page received")
         order = request.website.sale_get_order(force_create=1, context=context)
         #adapt when delivery method is changed
         carrier_id = post.get('carrier_id')
@@ -24,58 +24,61 @@ class website_sale(openerp.addons.website_sale.controllers.main.website_sale):
             carrier_id = int(carrier_id)
             _logger.debug("carrierID : %d", carrier_id)
         if order:
+            _logger.debug("orderID : %d", order.id)
             request.registry['sale.order']._check_carrier_quotation(cr, uid, order, force_carrier_id=carrier_id, context=context)
             if carrier_id:
                 return request.redirect("/shop/checkout")
 
         redirection = self.checkout_redirection(order)
         if redirection:
+            _logger.debug("checkout : redirection")
             return redirection
 
         values = self.checkout_values()
-        #need delivery informations
+        #need delivery informations #TODO : overload checkout_values instead of adding get_website_data
         sale_order_obj = request.registry.get('sale.order')
         values.update(sale_order_obj._get_website_data(cr, uid, order, context))
         #need shipping informations of the shop
-        order = request.website.sale_get_order(force_create=1, context=context)
+        #order = request.website.sale_get_order(force_create=1, context=context)
         _logger.debug("order.carrier_id : %d", order.carrier_id)
         _logger.debug("order.carrier_id.name : %s", order.carrier_id.name)
-        if(order.carrier_id.name.find("Tong") != -1) :
-            _logger.debug("Adding Tongres address")
-            values['checkout']['shipping_id'] = -1
-            values['checkout']['shipping_name'] = "Tongres"
-            values['checkout']['shipping_phone'] = "02 734 08 02"
-            values['checkout']['shipping_street'] = "107, Rue Gérard"
-            values['checkout']['shipping_city'] = "Etterbeek"
-            values['checkout']['shipping_zip'] = "1040"
-            values['checkout']['shipping_country_id'] = 21
-        elif(order.carrier_id.name.find("Wolu") != -1) :
-            _logger.debug("Adding Wolu address")
-            values['checkout']['shipping_id'] = -1
-            values['checkout']['shipping_name'] = "Woluwé"
-            values['checkout']['shipping_phone'] = "02 763 48 93"
-            values['checkout']['shipping_street'] = "27, Avenue Baron d'Huart"
-            values['checkout']['shipping_city'] = "Woluwé-Saint-Pierre"
-            values['checkout']['shipping_zip'] = "1150"
-            values['checkout']['shipping_country_id'] = 21
-        elif(order.carrier_id.name.find("Jaco") != -1) :
-            _logger.debug("Adding Uccle address")
-            values['checkout']['shipping_id'] = -1
-            values['checkout']['shipping_name'] = "Uccle"
-            values['checkout']['shipping_phone'] = "02 375 48 75"
-            values['checkout']['shipping_street'] = "1395, Chaussée de Waterloo"
-            values['checkout']['shipping_city'] = "Uccle"
-            values['checkout']['shipping_zip'] = "1180"
-            values['checkout']['shipping_country_id'] = 21
-        elif(order.carrier_id.name.find("ulpe") != -1) :
-            _logger.debug("Adding La Hulpe address")
-            values['checkout']['shipping_id'] = -1
-            values['checkout']['shipping_name'] = "La Hulpe"
-            values['checkout']['shipping_phone'] = " 02 652 21 93"
-            values['checkout']['shipping_street'] = "30, Avenue Albert 1er"
-            values['checkout']['shipping_city'] = "Genval"
-            values['checkout']['shipping_zip'] = "1332"
-            values['checkout']['shipping_country_id'] = 21
+        if(order.carrier_id) :
+            if(order.carrier_id.name.find("Tong") != -1) :
+                _logger.debug("Adding Tongres address")
+                values['checkout']['shipping_id'] = -1
+                values['checkout']['shipping_name'] = "Tongres"
+                values['checkout']['shipping_phone'] = "02 734 08 02"
+                values['checkout']['shipping_street'] = "107, Rue Gérard"
+                values['checkout']['shipping_city'] = "Etterbeek"
+                values['checkout']['shipping_zip'] = "1040"
+                values['checkout']['shipping_country_id'] = 21
+            elif(order.carrier_id.name.find("Wolu") != -1) :
+                _logger.debug("Adding Wolu address")
+                values['checkout']['shipping_id'] = -1
+                values['checkout']['shipping_name'] = "Woluwé"
+                values['checkout']['shipping_phone'] = "02 763 48 93"
+                values['checkout']['shipping_street'] = "27, Avenue Baron d'Huart"
+                values['checkout']['shipping_city'] = "Woluwé-Saint-Pierre"
+                values['checkout']['shipping_zip'] = "1150"
+                values['checkout']['shipping_country_id'] = 21
+            elif(order.carrier_id.name.find("Jaco") != -1) :
+                _logger.debug("Adding Uccle address")
+                values['checkout']['shipping_id'] = -1
+                values['checkout']['shipping_name'] = "Uccle"
+                values['checkout']['shipping_phone'] = "02 375 48 75"
+                values['checkout']['shipping_street'] = "1395, Chaussée de Waterloo"
+                values['checkout']['shipping_city'] = "Uccle"
+                values['checkout']['shipping_zip'] = "1180"
+                values['checkout']['shipping_country_id'] = 21
+            elif(order.carrier_id.name.find("ulpe") != -1) :
+                _logger.debug("Adding La Hulpe address")
+                values['checkout']['shipping_id'] = -1
+                values['checkout']['shipping_name'] = "La Hulpe"
+                values['checkout']['shipping_phone'] = " 02 652 21 93"
+                values['checkout']['shipping_street'] = "30, Avenue Albert 1er"
+                values['checkout']['shipping_city'] = "Genval"
+                values['checkout']['shipping_zip'] = "1332"
+                values['checkout']['shipping_country_id'] = 21
         return request.website.render("website_sale.checkout", values)
     
     def order_lines_2_google_api(self, order_lines):
