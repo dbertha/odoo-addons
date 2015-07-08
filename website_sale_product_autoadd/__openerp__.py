e6926fae95692606ff91fd45a1b02620465e3879
David Bertha <dbertha@ulb.ac.be>
2015-07-08 14:18:06 +0200

    new module : product companion automatically added/removed with a quantity as the sum of the quantities of products in cart which have this product companion

---
+++ website_sale_product_autoadd/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Products automatically added with other products",
+
+    'summary': 
+    """When a product is added to the cart, its associated products are automatically added""",
+
+    'description': 
+    """Can be used for mandatory warranty, cash security, ...""",
+
+    'author': "David Bertha",
+    'website': "",
+
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base', 'website', 'website_sale', 'product'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+        'views.xml'
+    ],
+
+    # only loaded in demonstration mode
+    'demo': [],
+}
