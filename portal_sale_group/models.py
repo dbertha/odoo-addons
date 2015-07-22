d1cd195e58e5ed15f351c9b238cdafbbb260d274
David Bertha <dbertha@ulb.ac.be>
2015-07-22 14:35:36 +0200

    fix : filter a tree view of one2many attr in group form

--- portal_sale_group/models.py
+++ portal_sale_group/models.py
@@ -17,12 +17,21 @@ class PortalGroup(osv.osv) :
             res[group.id] = len(group.members_ids)
         return res
     
+    def _get_filtered_orders(self, cr, uid, ids, field_name, arg, context=None):
+        """Return the list of orders with a correct state"""
+        res = {}
+        for group in self.browse(cr, uid, ids, context=context):
+            so_ids = self.pool.get('sale.order').search(cr, uid, [('portal_group_id', '=', group.id), ('state', 'not in', ['draft', 'cancel'])])
+            res[group.id] = so_ids
+        return res
+    
     _columns = {
         'name' : fields.char(string="Name", help="Name of the group", required=True),
         'members_ids' : fields.one2many('res.users', 'portal_group_id', string="Users", help="The users in the group"),
         'nb_of_members' : fields.function(_compute_nb_of_members, store=True, type='integer', string="Number of members"),
         'administrator' : fields.many2one('res.users', string="Administrator", help="Member of the group who can add and remove members"),
         'order_ids' : fields.one2many('sale.order', 'portal_group_id', string="Orders", help="The orders from users in the group", readonly=True),
+        'filtered_order_ids' : fields.function(_get_filtered_orders,obj='sale.order', type="one2many", string="Orders", help="The orders from users in the group"),
         'delivery_condition' : fields.many2one('delivery.condition', string="Conditions of Delivery", ondelete="restrict"),
         'hour_of_delivery' : fields.selection([(9, "09"), (10,"10"), (11, "11"), (12, "12"), (13, "13"), (14, "14"), (15, "15")], default=10, required=True, string="Hour of delivery"),
         'product_id' : fields.many2one('product.product', string='Service Product', help="This product will be added to the invoice")
