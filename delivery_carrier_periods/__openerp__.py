fc149c1723a662de3041fffe8cbf8ed777b7fa89
David Bertha <dbertha@ulb.ac.be>
2015-07-22 13:52:27 +0200

    Delivery Periods : add security rule

--- delivery_carrier_periods/__openerp__.py
+++ delivery_carrier_periods/__openerp__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         #'templates.xml',
         #'delivery_report.xml',
         #'report_saleorder_delivery.xml',
