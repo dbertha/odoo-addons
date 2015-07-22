bd91033f81c17b3938024099c36427b9b0a3338d
David Bertha <dbertha@ulb.ac.be>
2015-07-22 13:53:56 +0200

    website_sale_rotating : cron method record

--- website_sale_rotating/__openerp__.py
+++ website_sale_rotating/__openerp__.py
@@ -8,6 +8,7 @@
     'description': """
         Products can be marked with a week number and displayed on e-shop only when
         current week is that number. That number is cycling.
+        The cron method for rotating should be configured and activated.
     """,
 
     'author': "David Bertha",
@@ -24,7 +25,8 @@
         #'security/ir.model.access.csv',
         'res_config_view.xml',
         'views.xml',
-        'templates.xml'
+        'templates.xml',
+        'schedular_data.xml'
     ],
 
     # only loaded in demonstration mode
