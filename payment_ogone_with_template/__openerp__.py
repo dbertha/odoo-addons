3226487215b355eac88e7efcefc8f31822fe543d
David Bertha <dbertha@ulb.ac.be>
2016-06-04 16:10:50 +0200

    new ogone parameter : template URL, with correct shasign

---
+++ payment_ogone_with_template/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Ogone Payment Acquirer : add template param',
+    'category': 'Hidden',
+    'summary': 'Payment Acquirer: add template URL as parameter with correct shasign',
+    'version': '1.0',
+    'description': """""",
+    'author': 'OpenERP SA',
+    'depends': ['payment_ogone'],
+    'data': [
+        'views/ogone.xml',
+        'views/payment_acquirer.xml',
+    ],
+    'installable': True,
+}
