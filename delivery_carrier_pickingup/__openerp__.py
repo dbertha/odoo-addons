bf16f01aa1139c9c481b480b13a6f18957e2741f
dbertha <david.bertha@gmail.com>
2017-03-22 18:11:11 +0100

    migration to v9, using partner of stock location instead of any partner, simplifying data processing during checkout

--- delivery_carrier_pickingup/__openerp__.py
+++ delivery_carrier_pickingup/__openerp__.py
@@ -3,28 +3,28 @@
     'name': "Picking Address with Delivery Method",
 
     'summary': """
-        Add a partner field in delivery method""",
+        Add a stock location field in delivery method""",
 
     'description': """
-        Add a partner field in delivery method. The address of the partner
-        will be used as shipping address on webshop
+        Add a stock location field in delivery method. The partner of the location
+        will be used as shipping address on webshop when this carrier is chosen
     """,
 
     'author': "David Bertha",
     'website': "",
-    'installable' : False,
+    'installable' : True,
 
     'category': 'Uncategorized',
     'version': '0.1',
 
-    'depends': ['base', 'website', 'website_sale'], 
+    'depends': ['base', 'website', 'website_sale', 'delivery'], 
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'templates.xml',
-        'delivery_report.xml',
-        'report_saleorder_delivery.xml',
+        # 'delivery_report.xml',
+        # 'report_saleorder_delivery.xml',
         'views.xml'
     ],
 
--- delivery_carrier_pickingup/models.py
+++ delivery_carrier_pickingup/models.py
@@ -1,20 +1,33 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, SUPERUSER_ID
-from openerp.osv import osv, fields
+from openerp import models, fields, SUPERUSER_ID
 import logging
 
 _logger = logging.getLogger(__name__)
 
 
-class delivery_carrier(osv.osv) :
+class DeliveryCarrier(models.Model) :
     _name = "delivery.carrier"
     _inherit = "delivery.carrier"
-    _columns = {
-        'is_pickup' : fields.boolean(string='Is a shop pick-up', 
-            help='if activated, the address of delivery_method will be used as shipping address'),
-        'address_partner' : fields.many2one('res.partner', string="Address", 
+
+    is_pickup = fields.Boolean(string='Is a shop pick-up', 
+            help='if activated, the address of delivery_method will be used as shipping address')
+    shop_location = fields.Many2one('stock.location', string="Stock Location", 
+            help="If it's a picking in shop, the address of the stock will be used as shipping address. "+ 
+            "If no pickup, this field can still be used to store an email address to be notified when this delivery carrier is used")
+    address_partner = fields.Many2one('res.partner', related ="shop_location.partner_id", store=True, string="Address", 
             help="Address to use as shipping address."+ 
             "If no pickup, this field can still be used to store an email address to be notified when this delivery carrier is used")
-    }
     
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def delivery_set(self):
+    	result = super(SaleOrder, self).delivery_set()
+    	if result :
+    		for order in self: 
+    			carrier = order.carrier_id
+    			if carrier.is_pickup and carrier.address_partner :
+    				order.partner_shipping_id = carrier.address_partner.id
+    	return result
