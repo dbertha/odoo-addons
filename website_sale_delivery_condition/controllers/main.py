34d12490fc53e6036566e375866d4877b85c1d32
dbertha <david.bertha@gmail.com>
2017-04-25 14:53:47 +0200

    fix args

--- website_sale_delivery_condition/controllers/main.py
+++ website_sale_delivery_condition/controllers/main.py
@@ -122,7 +122,7 @@ class website_sale(openerp.addons.website_sale.controllers.main.website_sale):
         delivery_condition = request.website.sale_get_delivery_condition()
 
         product_template_objs = pool['product.template']
-        current_week = product_template_objs.get_current_week(cr, uid, [0], context=None)
+        current_week = product_template_objs.get_current_week()
          
 
         values = {
