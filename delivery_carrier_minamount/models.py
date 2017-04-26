9890deda717fa0211ccec29888f12da469262250
dbertha <david.bertha@gmail.com>
2017-04-26 09:40:27 +0200

    fix old api

--- delivery_carrier_minamount/models.py
+++ delivery_carrier_minamount/models.py
@@ -20,7 +20,7 @@ class DeliveryCarrier(models.Model):
             SaleOrder = self.env['sale.order']
             order = SaleOrder.browse(order_id)
 
-            if self.amount_lower_bound > ((order.amount_total - order.amount_delivery) or 0.0) :
+            if self.amount_lower_bound > (order.amount_total - order.amount_delivery) :
                 self.available = False
 
     
